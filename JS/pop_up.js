4704005f5a8d60e28e7a692b5e844c8e795f189f
bae <bms030212@naver.com>
2025-04-09 13:55:40 +0900

    6주차 강의실습 완료

--- JS/pop_up.js
+++ JS/pop_up.js
@@ -1,5 +1,5 @@
-function pop_up() {
-    window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+    function pop_up() {
+        window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     } // 여기서 ../을 하는 이유는 상위의 상위폴더에서 찾기 위해서이다!
 
     function show_clock(){
@@ -7,18 +7,25 @@ function pop_up() {
         let divClock = document.getElementById('divClock');
         let msg = "현재 시간 : ";
         if(currentDate.getHours()>12){ // 12시 보다 크면 오후 아니면 오전
-        msg += "오후";
-        msg += currentDate.getHours()-12+"시";
-        }
+            msg += "오후";
+            msg += currentDate.getHours()-12+"시";
+            }
         else {
-        msg += "오전";
-        msg += currentDate.getHours()+"시";
-        }
+            msg += "오전";
+            msg += currentDate.getHours()+"시";
+            }
         msg += currentDate.getMinutes()+"분";
         msg += currentDate.getSeconds()+"초";
         divClock.innerText = msg;
         if (currentDate.getMinutes()>58) { //정각 1분전 빨강색 출력
-        divClock.style.color="red";
-        }
+            divClock.style.color="red";
+            }
         setTimeout(show_clock, 1000); //1초마다 갱신
-        }
+    }
+    function over(obj) {
+        obj.src="lol/lol title.png";
+    }
+    function out(obj) {
+        obj.src="lol/lol title2.webp";
+    }
+            
